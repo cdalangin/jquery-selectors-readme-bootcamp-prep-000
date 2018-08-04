38a3f22ceef3f4fd9ee764c54b71bd8c3941d0ce
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-08-04 17:40:59 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -3,7 +3,7 @@ function paragraphSelector() {
   return $('p')
 }
 function lastImageSelector() {
-  return $('div img:last-child')
+  return $('div:last-child')
 }
 function ninjaBabySelector() {
   return $('#baby-ninja')
